73acbf75291158ed9eb8fe060fa75d550def79f7
淼淼 <miaomiao@tongbanjie.com>
2019-12-06 18:42:08 +0800

    bytecode

---
+++ src/main/java/com/miaomiao/classLoad/bytecode/MyTest1.java
@@ -0,0 +1,18 @@
+package com.miaomiao.classLoad.bytecode;
+
+
+/**
+ *  16进制 转10进制
+ *  34 = 3*16+4 = 52   18 = 16*1+8 = 24
+ */
+public class MyTest1 {
+    private int a =1;
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+}
